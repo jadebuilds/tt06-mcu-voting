5be6192e1ab0c3f2c25434c9fcb139d644cdc9de
Jade <jade@rootsystems.net>
2024-04-14 16:14:48 -0700

    Reinstate test harness content from example
    Maybe we need a clock? It'll show up in the .tcd?

--- test/test.py
+++ test/test.py
@@ -6,11 +6,29 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
-async def test_all_ones(dut):
+async def test_project(dut):
   dut._log.info("Start")
   
-  dut.ui_in.value = 0b11111111
-  
+  # We don't use the clock currently but we're gonna invoke it anyway just in case
+  clock = Clock(dut.clk, 10, units="us")
+  cocotb.start_soon(clock.start())
+
+  # We don't really use these either??? but who knows
+  dut._log.info("Reset")
+  dut.ena.value = 1
+  dut.ui_in.value = 0
+  dut.uio_in.value = 0
+  dut.rst_n.value = 0
+  await ClockCycles(dut.clk, 10)
+  dut.rst_n.value = 1
+
+  # Yea why not let's see some clock cycles in the .tcd 
+
+  # Set the input values, wait one clock cycle, and check the output
+  dut._log.info("Test")
+  dut.ui_in.value = 20
+  dut.uio_in.value = 30
+
   await ClockCycles(dut.clk, 1)
 
-  assert dut.uo_out.value == 0b00000001
+  assert dut.uo_out.value == 50
