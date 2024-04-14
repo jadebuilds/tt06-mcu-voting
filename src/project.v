766eb6a15b6dc7008df0572de9708dde94d962ea
Shahzoda Davlatova <shahzoda01@gmail.com>
2024-04-14 15:17:52 -0700

    First interation - simple 8 input pins and single OR logic gate output between them all.

--- src/project.v
+++ src/project.v
@@ -16,8 +16,22 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  // some processing! 
+  // all the input becomes a part of our voting system 
+  assign wire voter0 = ui_in[0];
+  assign wire voter1 = ui_in[1];
+  assign wire voter2 = ui_in[2];
+  assign wire voter3 = ui_in[3];
+  assign wire voter4 = ui_in[4];
+  assign wire voter5 = ui_in[5];
+  assign wire voter6 = ui_in[6];
+  assign wire voter7 = ui_in[7];
+
+  // then we do a an "or" and assign to the first register of the output
+  assign uo_out[0] = voter0 | voter1 | voter2 | voter3 | voter4 | voter5 | voter6 | voter7;
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:1]  = 0  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
